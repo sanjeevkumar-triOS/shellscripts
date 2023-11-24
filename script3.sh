5acbacfd92c7b59e7e7217ed6c40e25df3aab9bd
sanjeevkumar-triOS <145776447+sanjeevkumar-triOS@users.noreply.github.com>
2023-11-24 11:43:56 -0500

    Add files via upload

---
+++ script3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This script backs up a directory specified by the first argument (positional parameter)
+if [ $# -ne 1 ]
+then 
+echo "Usage is $0 <directory to back up>"
+exit 255
+fi
+
+echo "Performing backup....."
+sleep 3
+tar -zcvf ~/backupfile.tar.gz $1
+
+echo "Backup completed successfully to ~/backupfile.tar.gz"
